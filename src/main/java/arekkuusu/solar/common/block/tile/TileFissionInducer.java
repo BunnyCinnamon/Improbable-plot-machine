460948d87922dbf3c66430abc8a6352b2a8edda8
ArekkuusuJerii <edgargcx99@gmail.com>
2018-04-13 22:33:49 -0500

    Tweaked Luminic Mechanism behavior

--- src/main/java/arekkuusu/solar/common/block/tile/TileFissionInducer.java
+++ src/main/java/arekkuusu/solar/common/block/tile/TileFissionInducer.java
@@ -10,6 +10,7 @@ package arekkuusu.solar.common.block.tile;
 import arekkuusu.solar.client.effect.FXUtil;
 import arekkuusu.solar.common.block.ModBlocks;
 import arekkuusu.solar.common.block.fluid.ModFluids;
+import com.google.common.collect.ImmutableMap;
 import net.katsstuff.mirror.client.particles.GlowTexture;
 import net.katsstuff.mirror.data.Vector3;
 import net.minecraft.block.Block;
@@ -17,9 +18,12 @@ import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
@@ -31,6 +35,7 @@ import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 
 import javax.annotation.Nullable;
 import java.util.Arrays;
+import java.util.Map;
 
 /**
  * Created by <Arekkuusu> on 4/5/2018.
@@ -38,18 +43,37 @@ import java.util.Arrays;
  */
 public class TileFissionInducer extends TileBase implements ITickable {
 
-	private static final BlockPos[] POSITIONS = {
-			new BlockPos(1, 0, 1 ),
-			new BlockPos(-1, 0, -1 ),
-			new BlockPos(1, 0, 0 ),
-			new BlockPos(0, 0, 1 ),
-			new BlockPos(-1, 0, 0 ),
-			new BlockPos(0, 0, -1 ),
-			new BlockPos(-1, 0, 1 ),
-			new BlockPos(1, 0, -1 )
-	};
-	public static final int MAX_CAPACITY = 10000;
-	private int[] ticks = new int[POSITIONS.length];
+	private final Map<EnumFacing.Axis, BlockPos[]> POSITIONS = ImmutableMap.<EnumFacing.Axis, BlockPos[]>builder()
+			.put(EnumFacing.Axis.Z, new BlockPos[]{
+					new BlockPos(0, 1, 0),
+					new BlockPos(1, 1, 0),
+					new BlockPos(-1, 1, 0),
+					new BlockPos(0, -1, 0),
+					new BlockPos(1, -1, 0),
+					new BlockPos(-1, -1, 0),
+					new BlockPos(1, 0, 0),
+					new BlockPos(-1, 0, 0)
+			}).put(EnumFacing.Axis.X, new BlockPos[]{
+					new BlockPos(0, 1, 0),
+					new BlockPos(0, 1, 1),
+					new BlockPos(0, 1, -1),
+					new BlockPos(0, -1, 0),
+					new BlockPos(0, -1, 1),
+					new BlockPos(0, -1, -1),
+					new BlockPos(0, 0, 1),
+					new BlockPos(0, 0, -1)
+			}).put(EnumFacing.Axis.Y, new BlockPos[]{
+					new BlockPos(1, 0, 1),
+					new BlockPos(-1, 0, -1),
+					new BlockPos(1, 0, 0),
+					new BlockPos(0, 0, 1),
+					new BlockPos(-1, 0, 0),
+					new BlockPos(0, 0, -1),
+					new BlockPos(-1, 0, 1),
+					new BlockPos(1, 0, -1)
+			}).build();
+	public static final int MAX_CAPACITY = 8000;
+	private int[] ticks = new int[8];
 	private final FluidTank handler;
 	private int tick;
 
@@ -62,41 +86,50 @@ public class TileFissionInducer extends TileBase implements ITickable {
 	public void update() {
 		if(!world.isRemote) {
 			if(tick++ % 20 == 0 && canFill()) {
-				for(int i = 0; i < POSITIONS.length; i++) {
-					BlockPos position = POSITIONS[i].add(getPos());
+				BlockPos[] positions = POSITIONS.get(getFacingLazy().getAxis());
+				boolean empty = false;
+				for(int i = 0; i < positions.length; i++) {
+					BlockPos position = positions[i].add(getPos());
 					IBlockState state = world.getBlockState(position);
 					if(state.getBlock() == Blocks.GOLD_BLOCK) {
-						if(ticks[i]++ >= 10) {
-							world.playEvent(2001, position, Block.getStateId(state));
+						if(ticks[i]++ >= 2) {
 							world.setBlockState(position, ModBlocks.MOLTEN_GOLD.getDefaultState());
 							ticks[i] = -1;
 						}
 					} else if(state.getBlock() == ModBlocks.MOLTEN_GOLD) {
-						if(ticks[i]++ >= 25) {
+						if(ticks[i]++ >= 15) {
 							world.playEvent(2001, position, Block.getStateId(state));
 							FluidStack stack = new FluidStack(ModFluids.GOLD, Fluid.BUCKET_VOLUME);
 							((IFluidBlock) ModFluids.GOLD.getBlock()).place(world, position, stack, true);
 							ticks[i] = -1;
 						}
-					} else if(state.getBlock() instanceof IFluidBlock && world.rand.nextFloat() < 0.25F) {
+					} else if(state.getBlock() instanceof IFluidBlock && ticks[i]++ >= 1) {
 						IFluidBlock f = (IFluidBlock) state.getBlock();
 						if(f.getFluid() == ModFluids.GOLD && f.canDrain(world, position)) {
 							FluidStack stack = f.drain(world, position, false);
 							if(stack != null && stack.amount == Fluid.BUCKET_VOLUME) {
 								handler.fill(f.drain(world, position, true), true);
+								empty = true;
 							}
 						}
+						ticks[i] = -1;
 					}
+				}
+				if(empty) {
+					world.playSound(null, getPos(), SoundEvents.ITEM_BUCKET_EMPTY_LAVA, SoundCategory.BLOCKS, 1F, 1F);
 				}
 			}
 			applyGravity();
-		} else for(BlockPos position : POSITIONS) {
-			IBlockState state = world.getBlockState(position.add(getPos()));
-			if(state.getBlock() == Blocks.GOLD_BLOCK || state.getBlock() == ModBlocks.MOLTEN_GOLD) {
-				Vector3 pos = new Vector3.WrappedVec3i(position.add(getPos())).asImmutable().add(Math.random(), 0.75, Math.random());
-				Vector3 speed = Vector3.apply(0, 0.05, 0).multiply(world.rand.nextFloat());
-				int color = world.rand.nextBoolean() ? 0xff5000 : 0xfff200;
-				FXUtil.spawnTunneling(world, pos, speed, 20, 1F, color, GlowTexture.STAR);
+		} else {
+			BlockPos[] positions = POSITIONS.get(getFacingLazy().getAxis());
+			for(BlockPos position : positions) {
+				IBlockState state = world.getBlockState(position.add(getPos()));
+				if(state.getBlock() == Blocks.GOLD_BLOCK || state.getBlock() == ModBlocks.MOLTEN_GOLD) {
+					Vector3 pos = new Vector3.WrappedVec3i(position.add(getPos())).asImmutable().add(Math.random(), 0.75, Math.random());
+					Vector3 speed = Vector3.apply(0, 0.05, 0).multiply(world.rand.nextFloat());
+					int color = world.rand.nextBoolean() ? 0xff5000 : 0xfff200;
+					FXUtil.spawnTunneling(world, pos, speed, 20, 1F, color, GlowTexture.STAR);
+				}
 			}
 		}
 	}
