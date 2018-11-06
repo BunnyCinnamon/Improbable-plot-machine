b2765968769286702d031a9455e99d6a8a953136
ArekkuusuJerii <edgargcx99@gmail.com>
2018-11-05 23:19:04 -0600

    Correct Item TRSRTransforms

--- src/main/java/arekkuusu/implom/client/util/baker/model/ModelClockwork.java
+++ src/main/java/arekkuusu/implom/client/util/baker/model/ModelClockwork.java
@@ -5,6 +5,7 @@ import arekkuusu.implom.client.util.ResourceLibrary;
 import arekkuusu.implom.client.util.baker.baked.BakedClockwork;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import net.katsstuff.teamnightclipse.mirror.client.baked.BakedPerspective;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.block.model.BakedQuad;
@@ -19,7 +20,6 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ItemLayerModel;
-import net.minecraftforge.client.model.PerspectiveMapWrapper;
 import net.minecraftforge.client.model.SimpleModelState;
 import net.minecraftforge.common.model.IModelState;
 import net.minecraftforge.common.model.TRSRTransformation;
@@ -31,6 +31,16 @@ import java.util.function.Function;
 
 public class ModelClockwork implements IModel {
 
+	public static final ImmutableMap<ItemCameraTransforms.TransformType, TRSRTransformation> ITEM_TRANSFORMS = ImmutableMap.<ItemCameraTransforms.TransformType, TRSRTransformation>builder()
+			.put(ItemCameraTransforms.TransformType.GUI, BakedPerspective.mkTransform(0F, 0F, 0F, 0F, 0F, 0F, 1F))
+			.put(ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, BakedPerspective.mkTransform(3.6F, 6.6F, 4.6F, 0F, 0F, 0F, 0.55F))
+			.put(ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, BakedPerspective.mkTransform(3.6F, 6.6F, 4.6F, 0F, 0F, 0F, 0.55F))
+			.put(ItemCameraTransforms.TransformType.FIRST_PERSON_RIGHT_HAND, BakedPerspective.mkTransform(14.57F, 4F, 6.5F, 0F, -90F, 25F, 0.68F))
+			.put(ItemCameraTransforms.TransformType.FIRST_PERSON_LEFT_HAND, BakedPerspective.mkTransform(14.57F, 4F, 6.5F, 0F, -90F, 25F, 0.68F))
+			.put(ItemCameraTransforms.TransformType.GROUND, BakedPerspective.mkTransform(4F, 6F, 4F, 0F, 0F, 0F, 0.5F))
+			.put(ItemCameraTransforms.TransformType.FIXED, BakedPerspective.mkTransform(0F, 0F, 0F, 0F, 0F, 0F, 1F))
+			.put(ItemCameraTransforms.TransformType.HEAD, BakedPerspective.mkTransform(0F, 13F, 7F, 0F, 0F, 0F, 1F))
+			.build();
 	private final ImmutableList<ResourceLocation> textures;
 	private final ItemOverrideList overrides;
 
@@ -48,6 +58,7 @@ public class ModelClockwork implements IModel {
 
 	@Override
 	public IBakedModel bake(IModelState state, VertexFormat format, Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter) {
+		ImmutableMap<ItemCameraTransforms.TransformType, TRSRTransformation> transformMap = ImmutableMap.copyOf(ITEM_TRANSFORMS);
 		ImmutableList.Builder<BakedQuad> builder = ImmutableList.builder();
 		Optional<TRSRTransformation> transform = state.apply(Optional.empty());
 		for(int i = 0; i < textures.size(); i++) {
@@ -55,8 +66,7 @@ public class ModelClockwork implements IModel {
 			builder.addAll(ItemLayerModel.getQuadsForSprite(i, sprite, format, transform));
 		}
 		TextureAtlasSprite particle = bakedTextureGetter.apply(textures.isEmpty() ? new ResourceLocation("missingno") : textures.get(0));
-		ImmutableMap<ItemCameraTransforms.TransformType, TRSRTransformation> map = PerspectiveMapWrapper.getTransforms(state);
-		return new BakedClockwork(this, builder.build(), particle, map, overrides, format);
+		return new BakedClockwork(this, builder.build(), particle, transformMap, overrides, format);
 	}
 
 	@Override
